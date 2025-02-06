82ac1b67409288dfb1ed441fed967b276c0bf90d
Puja Borse <pujaborse13@gmail.com>
2025-02-07 00:11:07 +0530

    Calculate Area of Rectangle

---
+++ CalculateRectangleArea.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_UsingCSharp
+{
+    /* Rectangle Area Calculation
+        Create a Rectangle class with private fields for Length and Breadth.Add public methods to set
+        the values of these fields and a method to calculate and return the area of the rectangle.
+    */
+    internal class CalculateRectangleArea
+    {
+        private double length;
+        private double breadth;
+
+        public void SetLength(double length)
+        {
+            if (length > 0)
+            {
+                this.length = length;
+            }
+            else
+            {
+                Console.WriteLine("Length must be a positive number.");
+            }
+        }
+        public void SetBreadth(double breadth)
+        {
+            if (breadth > 0)
+            {
+                this.breadth = breadth;
+            }
+            else
+            {
+                Console.WriteLine("Breadth must be a positive number.");
+            }
+        }
+
+        public void CalculateArea()
+        {
+            double area = length * breadth;
+            Console.WriteLine($"Area of the Rectangle is: {area}");
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,24 @@
-﻿class Program
+﻿using OOP_UsingCSharp;
+
+class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Object Oriented Programming");
+
+        CalculateRectangleArea rect = new CalculateRectangleArea();
+
+
+        Console.Write("Enter Length of Rectangle: ");
+        double lengthInput = Convert.ToDouble(Console.ReadLine());
+        rect.SetLength(lengthInput);
+
+        Console.Write("Enter Breadth of Rectangle: ");
+        double breadthInput = Convert.ToDouble(Console.ReadLine());
+        rect.SetBreadth(breadthInput);
+
+        
+        rect.CalculateArea();
+
     }
 }
